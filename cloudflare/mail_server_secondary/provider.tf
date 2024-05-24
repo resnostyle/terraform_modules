015ecfbe153b13a7af882e6b87b10e914deadba5
B Pearson <git@me.resnostyle.dev>
2024-05-24 18:28:41 -0400

    use a symlink to make things easier

--- cloudflare/mail_server_secondary/provider.tf
+++ cloudflare/mail_server_secondary/provider.tf
@@ -1,14 +1,1 @@
-# Configure the Cloudflare provider.
-# You may optionally use version directive to prevent breaking changes occurring unannounced.
-terraform {
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "4.33.0"
-    }
-    namecheap = {
-      source  = "namecheap/namecheap"
-      version = ">= 2.0.0"
-    }
-  }
-}
+provider.tf
