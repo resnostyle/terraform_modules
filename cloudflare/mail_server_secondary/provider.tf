06bc90e5c1b527c1217802fa2f2cff93c836d20e
Bryan Pearson <bwp.pearson@gmail.com>
2024-07-11 08:55:16 -0400

    no longer requiring versions to make updates easier (#89)
    * no longer requiring versions to make updates easier

--- cloudflare/mail_server_secondary/provider.tf
+++ cloudflare/mail_server_secondary/provider.tf
@@ -1,14 +1,12 @@
-# Configure the Cloudflare provider.
-# You may optionally use version directive to prevent breaking changes occurring unannounced.
 terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "4.37.0"
+      version = "~>4.0"
     }
     namecheap = {
       source  = "namecheap/namecheap"
-      version = ">= 2.0.0"
+      version = "~> 2.0"
     }
   }
 }
